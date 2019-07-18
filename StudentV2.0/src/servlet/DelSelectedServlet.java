b618bf7d119d3e6c7b0139ef0707ac154138a98a
JayW124 <1242797001@qq.com>
2019-07-18 20:43:17 +0800

    README.md

---
+++ StudentV2.0/src/servlet/DelSelectedServlet.java
@@ -0,0 +1,14 @@
+/**
+ * Created by wangjie on 2019/7/18
+ * Descriptions:
+ */
+@javax.servlet.annotation.WebServlet("/DelSelectedServlet")
+public class DelSelectedServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+        this.doPost(request, response);
+    }
+}
